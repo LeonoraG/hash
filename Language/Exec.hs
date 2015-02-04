313f733b5d47e280e09221f7fcea8633a5e2381b
Leonora Gašpar <leonora.gaspar13@gmail.com>
2015-02-04 01:13:08 +0100

    Folder structure reorganisation

--- Language/Exec.hs
+++ Language/Exec.hs
@@ -1,3 +1,20 @@
+module Language.Exec where
+
+import Language.Expressions
+import Parsing.HashParser
+
+import Control.Exception
+import qualified Data.Map as M
+import Data.Maybe
+import System.IO
+import Control.Monad
+import Text.Parsec.String
+import Language.Expressions
+import Text.ParserCombinators.Parsec
+import Control.Applicative ( (<$>), (<*>), (<*) )
+import Data.List
+import Prelude hiding (catch) 
+
 -- A model of a command which is waiting for arguments and a state to run
 type Command  = [String] -> ScriptState -> IO ScriptState
 
@@ -5,7 +22,7 @@ type Command  = [String] -> ScriptState -> IO ScriptState
 type VarTable = M.Map String String
 
 -- A command table - abstracted command execution, (contains command name,
--- command) pairs. Simplest, but hardly the best way to implement this.
+-- command) pairs.
 type CommandTable = M.Map String Command
 
 -- A script state containing the last output, current working directory and
@@ -15,21 +32,136 @@ data ScriptState = ScriptState { output   :: String
                                , vartable :: VarTable
                                } deriving Show
 
+-- Calculates the result of a top-level command execution
+runTopLevel :: CommandTable -> TLExpr -> ScriptState -> IO ScriptState
+runTopLevel ctable tlexpr sstate  = case tlexpr of
+                                        TLCmd cmd  -> evalCmd   ctable cmd  sstate 
+                                        TLCnd cond -> evalCond  ctable sstate cond
+                                        
+evalTLList :: CommandTable -> ScriptState -> [TLExpr] -> IO ScriptState
+evalTLList _       sstate []   = return sstate
+evalTLList ctable sstate ltlexpr = foldr (flip (>>=) ) (return sstate) tlexprWithCtable
+  where tlexprWithCtable = map (runTopLevel ctable) ltlexpr
+        
 -- Runs a set of commands for a given command table. If this is the first
 -- command in the chain, it is given a FilePath and constructs a new, initially
 -- blank, ScriptState. Otherwise, it is given the state as left by the previous
 -- command’s execution.
+
 runHashProgram :: CommandTable -> Either FilePath ScriptState -> [TLExpr]
                   -> IO ScriptState
-
--- Calculates the result of a top-level command execution
-runTopLevel :: CommandTable -> ScriptState -> TLExpr -> IO ScriptState
-
---The rest of the module should consist of similar functions, calling each
---other so that each expression is parsed by a lower-level function and the
---result can be used in a higher-level function. The Command table and state
---are passed around as necessary to evaluate commands, assignments and
---variable substitution. A better way to pass around variables would be to
---use the State monad or even the StateT monad transformer to wrap IO into it.
+runHashProgram ctable eitherSstate ltlexpr = do
+  let sstate = case eitherSstate of
+                Left fp -> ScriptState { output = "", wd = fp, vartable = M.empty }
+                Right s -> s
+  evalTLList ctable sstate ltlexpr
 
 
+-- Transforms an Expr into it's value, using a VarTable vartable
+evalExpr :: Expr -> VarTable -> String
+evalExpr expr vartable = 
+    case expr of
+         Var v -> case M.lookup v vartable of
+                      Nothing -> ""
+                      Just val -> val 
+         Str s -> s
+         
+-- Evaluates comparison operator
+evalComp :: Comp -> VarTable -> Bool
+evalComp comp vartable = 
+    case comp of
+         CEQ a b -> (val a == val b)
+         CNE a b -> (val a /= val b)
+         CGE a b -> (val a >= val b)
+         CGT a b -> (val a >  val b)
+         CLE a b -> (val a <= val b)
+         CLT a b -> (val a <  val b)
+         CLI a   -> (val a == "")
+    where val a = evalExpr a vartable
+          
+-- Evaluates predicates
+evalPred :: Pred -> VarTable -> Bool
+evalPred pred vartable = 
+    case pred of
+         Pred a   -> evalComp a vartable
+         Not a    -> not $ val a
+         And a b  -> val a && val b
+         Or  a b  -> val a || val b
+         Parens a -> val a
+    where val pred = evalPred pred vartable
+          
+-- Evaluates assingment command
+evalAssign :: Cmd -> ScriptState -> ScriptState
+evalAssign asgn sstate =
+    sstate {vartable = newvartable}
+      where 
+        Var key            = var asgn
+        Str value          = val asgn
+        table        = vartable sstate
+        newvartable  = M.insert key value table
+
+-- Converts a expr filepath into an absolute FilePath, using the working directory file path
+-- from sstate. Working directory must end with a /
+-- Alternative: use System.IO functions that get and set current directory
+
+evalFp :: Expr -> ScriptState -> FilePath
+evalFp expr sstate = evalExpr expr (vartable sstate)
+
+-- For catching unknown command error
+
+
+catchUnknownComm :: ScriptState ->  SomeException -> IO ScriptState
+catchUnknownComm sstate e = do
+    putStrLn $ show e  
+    return sstate
+    
+-- Evaluates a non-assingment command
+
+evalrealCmd :: CommandTable -> ScriptState -> Cmd -> IO ScriptState
+evalrealCmd ctable sstate cmd = do
+    let vtable = vartable sstate
+    let maybeCommand = M.lookup (evalExpr (name cmd)  vtable) ctable
+    let ourCommand = case maybeCommand of
+                          Nothing -> error "Unrecognized command"
+                          Just a  -> a
+    fargs <- if (isJust (inDir cmd)) then do
+                                        let fp = evalFp (fromJust $ inDir cmd) sstate
+                                        tempArgs <- exprFromFile fp
+                                        return $ case tempArgs of
+                                            Left err -> []
+                                            Right xs -> xs
+
+                                     else return []
+    let fargsEvaluated = map (`evalExpr` vtable ) fargs
+    let finalArgs = (map (`evalExpr` vtable ) $ args cmd) ++ fargsEvaluated
+    newsstate <- catch (ourCommand finalArgs sstate) $ catchUnknownComm (sstate{output =""}) 
+    let retOut = outDir cmd
+    case retOut of
+         Nothing -> putStr (output newsstate)
+         Just fp -> case append cmd of
+                  True -> appendFile (evalFp fp sstate) (output newsstate)
+                  _    -> writeFile  (evalFp fp sstate) (output newsstate)
+    return newsstate
+
+-- Evaluates the commands, according to its type
+
+evalCmd :: CommandTable -> Cmd -> ScriptState -> IO ScriptState
+evalCmd ctable cmd sstate  =
+  case cmd of
+       Assign _ _ -> return $ evalAssign cmd sstate
+       Cmd    _ _ _ _ _-> evalrealCmd ctable sstate cmd
+       
+evalCmdList :: CommandTable -> ScriptState -> [Cmd] -> IO ScriptState
+evalCmdList _      sstate []   = return sstate
+evalCmdList ctable sstate lcmd = foldr (flip (>>=) ) (return sstate) cmdsWithCtable
+    where cmdsWithCtable = map (evalCmd ctable) lcmd
+            
+evalCondIfThenElse :: CommandTable -> ScriptState -> Conditional -> IO ScriptState
+evalCondIfThenElse ctable sstate (IfElse cond1 cthen1 celse1) = if evalPred cond1 (vartable sstate) 
+                                                                   then evalCmdList ctable sstate cthen1
+                                                                else evalCmdList ctable sstate celse1
+                                
+evalCond :: CommandTable -> ScriptState -> Conditional -> IO ScriptState
+evalCond ctable sstate conditional = case conditional of
+                                          If cond1 cthen1 -> evalCondIfThenElse ctable sstate (IfElse {cond = cond1, cthen=cthen1, celse = []} )
+                                          IfElse cond1 cthen1 celse1 -> evalCondIfThenElse ctable sstate conditional
--- Parsing/HashParser.hs
+++ Parsing/HashParser.hs
@@ -1,47 +1,246 @@
 -- Contains the parsers that take a string and produce an executable list of
--- TLExpr constructs. We recommend Parsec for parsing.
+-- TLExpr constructs.
 
-import Text.ParserCombinators.Parsec
-import System.Environment
+module Parsing.HashParser where
+
 import Control.Monad
+import Control.Applicative ( (<$>), (<*>), (<*), (*>) )
+import Data.List
+import Language.Expressions
+import Text.ParserCombinators.Parsec
 
+-- Expr is a bottom-level expression
+-- It can be either a named variable or a string
 
-err = "An error has occurred"
+parseExpr :: Parser Expr
+parseExpr =  try varExp <|> strExpr
 
-simpleParse :: Parser a -> String -> Either ParseError a
-simpleParse p = parse p err
+-- Using usual syntax for variables : $...
 
--- expects syntax : if (something without spaces) then (something without spaces) fi
+varExp :: Parser Expr
+varExp = do
+    char '$'
+    first <- letter
+    rest  <- many alphaNum
+    return $ Var (first:rest)
 
-parseIf :: Parser [String]
-parseIf = do 
-          string "if "
-          cond <- many (noneOf " ")
-          string " then "
-          cthen <- many (noneOf " ")
-          string " fi"
-          return $ [cond,cthen]
+-- Tries to read enclosed, then escaped string
+-- If neither of this works, reads many characters that don't contain
+-- some special chars
 
--- expects syntax : if (something without spaces) then (something without spaces) else (something without spaces) fi
+strExpr :: Parser Expr
+strExpr = do         
+    x <- many1 (escapedChar<|>enclosedString<|>many1 (noneOf "\" \\\n\t;") )
+    return $ Str $ concat x
+    
+-- For strings between "
+enclosedString :: Parser String
+enclosedString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return x
 
-parseIfElse :: Parser [String]
-parseIfElse = do
-              string "if "
-              cond <- many (noneOf " ")
-              string " then "
-              cthen <- many (noneOf " ")
-              string " else "
-              celse <- many (noneOf " ")
-              string " fi"
-              return $ [cond,cthen,celse]
+-- For strings that begin with \\
+escapedChar :: Parser String
+escapedChar = do
+    char '\\'
+    a <- anyChar
+    return [a]
+ 
+
+-- Comparison parser
+
+parseComp :: Parser Comp
+parseComp = try compExpr <|> wrappedExpr
+
+-- When we have a wrapped expression literal
+wrappedExpr :: Parser Comp
+wrappedExpr = liftM CLI $ parseExpr
+    
+
+compExpr :: Parser Comp
+compExpr = do
+    x <- parseExpr
+    operator <-choice $ fmap (try.string) $ ["==","/=", ">", ">=",">","<=",""] 
+    y <- parseExpr
+    return $ case operator of
+              "==" ->  CEQ x y
+              "/=" ->  CNE x y
+              ">=" ->  CGE x y
+              ">"  ->  CGT x y
+              "<=" ->  CLE x y
+              "<"  ->  CLT x y
               
---parses a contition : something <,>,=, <=, /= something
+-- Parsing predicates
 
-parseCond :: Parser [String]
-parseCond = do
-            first <- many (noneOf "<>=/")
-            comp <- many (oneOf "<>=/")
-            second <- many (noneOf "<>=/")
-            return [first,comp,second]
+parsePred :: Parser Pred
+parsePred = try parensPred <|> try andOrPred <|> try notPred <|> wrappedComp
+
+notPred :: Parser Pred
+notPred =  char '!' >> (liftM Not $ parsePred)
+
+andOrPred :: Parser Pred
+andOrPred = do
+    a <- try parensPred <|> wrappedComp
+    symb <- choice $ fmap (try.string) $ ["&&", "||"]
+    b <- try parensPred <|> wrappedComp
+    return $ case symb of
+                  "&&" -> And a b
+                  "||" -> Or  a b
+
+parensPred :: Parser Pred
+parensPred = do
+  char '(' 
+  a <- parsePred
+  char ')'
+  return $ Parens a
+  
+-- For a wrapped comparison
+
+wrappedComp :: Parser Pred
+wrappedComp = liftM Pred $ parseComp
+  
+-- Commands can be either Cmd or an assingment
+
+parseCmd = try assign <|> realCmd  
+
+-- Assingns: var1 = val1
+assign :: Parser Cmd 
+assign = do
+    spaces
+    var1 <- varExp
+    char '='
+    val1 <- parseExpr
+    spaces
+    optional newline
+    return $ Assign { var = var1, val = val1}
+
+-- Real command consists of name and a list of arguments
+-- arguments can be separated by space or tab
+
+realCmd :: Parser Cmd 
+realCmd = do
+    spaces
+    name' <- parseExpr
+    spaces
+    args_ <- sepBy parseExpr (many $ char ' ' <|> tab)
+    spaces
+    let (args', inDir', outDir', append') = handleRedirects args_
+    spaces
+    optional newline
+    return $ Cmd { name   = name'
+                 , args   = args'
+                 , inDir  = inDir'
+                 , outDir = outDir'
+                 , append = append'
+                 }
+   
+-- Handling redirects
+handleRedirects :: [Expr] -> ( [Expr], Maybe Expr, Maybe Expr, Bool)
+handleRedirects args = outRedir $ inRedir args
+
+rmvIndexes :: [a] -> Int -> Int -> [a]
+rmvIndexes list arg1 arg2 = map snd $ filter (\x -> fst x /= arg1 && fst x /= arg2) $ indL
+  where indL = zip [0..] list
+
+inRedir :: [Expr] -> (Maybe Expr, [Expr])
+inRedir args = case findIndices (==Str "<") args of
+                    [] -> (Nothing, args)
+                    a  -> (Just $ args !! (last a + 1), rmvIndexes args (last a) (last a +1) )
+                   
+outRedir :: (Maybe Expr, [Expr]) -> ( [Expr], Maybe Expr, Maybe Expr, Bool)
+outRedir (inRedir, args) = case findIndices (== Str ">") args of
+                              [] -> case findIndices (== Str ">>") args of
+                                      [] -> (args, Nothing, inRedir, False)
+                                      a  -> (rmvIndexes args (last a) (last a + 1), inRedir, Just $ args !! (last a + 1) , True)
+                              a  -> (rmvIndexes args (last a) (last a + 1), inRedir, Just $ args !! (last a + 1) , False)
+                              
+
+                              
+-- Parses conditional branching expressions :
+-- if-then or if-then- else
+
+-- The syntax is similar to bash:
+-- if [condition]; then [expression]; else [expression]\n ; fi
+-- if [condition]; then [expression]; fi
+
+parseConditional :: Parser Conditional
+parseConditional = try ifThenElse <|> ifThen
+
+ifThen :: Parser Conditional
+ifThen = do
+    spaces
+    string "if"
+    spaces
+    cond2 <- parsePred
+    spaces
+    string "; then"
+    optional newline
+    then2 <- many (try parseCmd)
+    spaces
+    string "; fi"
+    return $ If { cond  = cond2
+                , cthen = then2 
+                }
+    
+ifThenElse :: Parser Conditional
+ifThenElse = do
+    spaces
+    string "if"   
+    spaces
+    cond2 <- parsePred
+    spaces
+    string "; then"  
+    optional newline
+    then2 <- many (try parseCmd)    
+    spaces    
+    string "; else"
+    spaces
+    else2 <- many (try parseCmd)
+    spaces
+    string "; fi"
+    return $ IfElse { cond  = cond2
+                    , cthen = then2
+                    , celse = else2
+                    }
 
 
+-- Parses a top level expression, which can be either
+-- conditional, or regular command
+
+parseTLExpr :: Parser TLExpr
+parseTLExpr = (TLCnd <$> try parseConditional) <|> (TLCmd <$> try parseCmd)
+
+exprFromFile :: String -> IO (Either ParseError [Expr])
+exprFromFile fp = parseFromFile (sepBy parseExpr (many $ char ' ' <|> char '\t')) fp
+
+tLExprsFromFile :: String -> IO (Either ParseError [TLExpr])
+tLExprsFromFile fp = parseFromFile (many $ skipMany parseComment >> parseTLExpr <* skipMany parseComment) fp
+
+parseTLExprsFromFile :: FilePath -> [String] -> IO [TLExpr]
+parseTLExprsFromFile fp args = do
+    con <- readFile fp --open the file 
+    let fline = head $ lines con --take the first line
+    let vars = parse (endBy varExp spaces) "variables" fline 
+    let (takesVars, varExprsList) = case vars of 
+                     Left _  -> (False, [])
+                     Right xs -> (True,  xs)
+    let body = case takesVars of
+                True  -> unlines $ tail $ lines con
+                False -> con
+    let extraTL = case takesVars of
+                True  -> map (\(v,arg) -> TLCmd $ Assign {var = v, val = Str arg} ) $ zip varExprsList args
+                False -> []
+    let ret = parse readTLExprAndComments "read script" body
+    return $ case ret of
+         Left  e -> error "Malformed script!"
+         Right tls -> extraTL ++ tls
+    
+readTLExprAndComments = endBy parseTLExpr spaces
+
+-- Parses out comments 
+-- comments are strings that begin with #
+
+parseComment :: Parser ()
+parseComment =  spaces >> char '#' >> many (noneOf "\n") >> optional newline >> return ()
